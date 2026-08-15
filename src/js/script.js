40cbe14f0345ccedd3271db2dc32779a5bcaf238
Inrtakt <vpavlenko205@gmail.com>
2026-08-15 10:54:01 +0300

    add order-modal

--- src/js/script.js
+++ src/js/script.js
@@ -22,3 +22,52 @@
 // // function handleClickHeroLink() {
 // //   backdrop.classList.add('is-open');
 // // }
+
+// Order modal
+const orderModalBackdrop = document.querySelector('.order-modal-backdrop');
+const orderModalOpenBtns = document.querySelectorAll('[data-order-modal-open]');
+const orderModalCloseBtn = document.querySelector('.order-modal-close-btn');
+const orderModalForm = document.querySelector('.order-modal-form');
+
+orderModalOpenBtns.forEach(btn =>
+  btn.addEventListener('click', handleOrderModalOpen)
+);
+orderModalCloseBtn.addEventListener('click', handleOrderModalClose);
+orderModalBackdrop.addEventListener('click', handleOrderModalBackdropClick);
+orderModalForm.addEventListener('submit', handleOrderModalSubmit);
+document.addEventListener('keydown', handleOrderModalEscape);
+
+function handleOrderModalOpen() {
+  orderModalBackdrop.classList.add('is-modal-open');
+  document.body.style.overflow = 'hidden';
+}
+
+function handleOrderModalClose() {
+  orderModalBackdrop.classList.remove('is-modal-open');
+  document.body.style.overflow = '';
+  orderModalForm.reset();
+  orderModalForm.classList.remove('is-validated');
+}
+
+function handleOrderModalBackdropClick(e) {
+  if (e.target === orderModalBackdrop) {
+    handleOrderModalClose();
+  }
+}
+
+function handleOrderModalEscape(e) {
+  const isOpen = orderModalBackdrop.classList.contains('is-modal-open');
+
+  if (e.key === 'Escape' && isOpen) {
+    handleOrderModalClose();
+  }
+}
+
+function handleOrderModalSubmit(e) {
+  e.preventDefault();
+  orderModalForm.classList.add('is-validated');
+
+  if (orderModalForm.checkValidity()) {
+    handleOrderModalClose();
+  }
+}
