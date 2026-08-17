c7631c8a7b78ac1e2393a503591f547368f19c44
Px228-Da-Da <160890712+Px228-Da-Da@users.noreply.github.com>
2026-08-17 15:08:14 +0300

    сделал полностью

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,31 @@
-// const burgerBtn = document.querySelector('.header-burher-btn');//кнопка яка відкриває мобільне меню
-// const mobileMenu = document.querySelector('.mobile-menu');//елемент до мобільного меню (div)
-// const closeBtn = document.querySelectorAll('.header-close-btn');//кнопка яка закриває мобільне меню
-// // const heroLink = document.querySelector('.hero-btn');// Линка в секції Hero дз
-// // const backdrop = document.querySelector('.backdrop');// backdrop для модалки з формою дз
-// const navLinks = document.querySelectorAll('.header-nav-list-item');//елементи списку навігації (li)
+// Mobile menu
+const burgerBtn = document.querySelector('.burger-button');
+const mobileMenuBackdrop = document.querySelector('.mobile-menu-backdrop');
+const mobileMenuCloseBtn = document.querySelector('.mobile-menu-close-btn');
+const mobileMenuLinks = document.querySelectorAll('.mobile-menu-link');
 
-// burgerBtn.addEventListener('click', handleClickBurgerBtn);
-// // heroLink.addEventListener('click', handleClickHeroLink);
+burgerBtn.addEventListener('click', handleMobileMenuOpen);
+mobileMenuCloseBtn.addEventListener('click', handleMobileMenuClose);
+mobileMenuLinks.forEach(link => link.addEventListener('click', handleMobileMenuClose));
+document.addEventListener('keydown', handleMobileMenuEscape);
 
-// function handleClickBurgerBtn() {
-//   mobileMenu.classList.add('is-open');
-// }
+function handleMobileMenuOpen() {
+  mobileMenuBackdrop.classList.add('is-open');
+  document.body.style.overflow = 'hidden';
+}
 
-// function handleClickCloseBtn(e) {
-//   mobileMenu.classList.remove('is-open');
-// //   backdrop.classList.remove('is-open');
-// }
-// navLinks.forEach((link) => link.addEventListener('click', handleClickCloseBtn));
-// closeBtn.forEach((link) => link.addEventListener('click', handleClickCloseBtn));
+function handleMobileMenuClose() {
+  mobileMenuBackdrop.classList.remove('is-open');
+  document.body.style.overflow = '';
+}
 
-// // function handleClickHeroLink() {
-// //   backdrop.classList.add('is-open');
-// // }
+function handleMobileMenuEscape(e) {
+  const isOpen = mobileMenuBackdrop.classList.contains('is-open');
+
+  if (e.key === 'Escape' && isOpen) {
+    handleMobileMenuClose();
+  }
+}
 
 // Order modal
 const orderModalBackdrop = document.querySelector('.order-modal-backdrop');
